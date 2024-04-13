09f2ea59f43716257d38463098c3e11154522a9b
khmylevdmitry <khmylev72@yandex.ru>
2024-04-13 12:02:23 +0700

    city parsing with sorting result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,38 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
         String csvFilePath = "src/main/resources/cities_list.csv";
         List<City> cities = readCitiesFromCSV(csvFilePath);
+        printCities(cities);
 
+        sortByCityNameDescending(cities);
+        printCities(cities);
+
+        sortByFederalDistrictAndCityNameDescending(cities);
+        printCities(cities);
+    }
+
+    public static void sortByCityNameDescending(List<City> cities) {
+        cities.sort(Comparator.comparing(City::getName, Comparator.reverseOrder()));
+    }
+
+    public static void sortByFederalDistrictAndCityNameDescending(List<City> cities) {
+        cities.sort(
+                Comparator.comparing(City::getDistrict)
+                        .thenComparing(City::getName, Comparator.reverseOrder())
+        );
+    }
+
+    private static void printCities(List<City> cities) {
         for (City city : cities) {
             System.out.println(city);
         }
+
     }
 
     private static List<City> readCitiesFromCSV(String filePath) {
@@ -24,7 +46,7 @@ public class Main {
             while (scanner.hasNextLine()) {
                 List<String> dynamicList = new ArrayList<>();
                 String line = scanner.nextLine();
-                String[] parts = line.split(";"); // Разделение строки по запятой
+                String[] parts = line.split(";");
 
                 for (int i = 0; i < parts.length; i++) {
                     dynamicList.add(parts[i]);
